7ce044b108b387aa67d5c2a52163186e41ddf3ac
Natasha <natasha.khoe@gmail.com>
2023-01-19 19:02:27 +0800

    commit tree c9c24a6f52d9b4ab71d19a42d84758fbc6ba51da

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
+    
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, include: :items
+    end
+
+    private
+    def render_record_not_found_response
+        render json: {error: "User Not Found"}, status: :not_found
+    end
+
 end
